888e88e86ff54c8b0347f63399d6e38a20696347
Brandon Arroyo <brandon.jarroyo@outlook.com>
2017-09-06 19:11:39 -0700

    added error log

--- server.js
+++ server.js
@@ -2,11 +2,16 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('./public'));
 
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
